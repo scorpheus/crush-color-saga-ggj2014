4406f27a46349c516121a6623e3bb86314ff059a
wawanbreton@gmail.com <wawanbreton@gmail.com@ccb83103-6311-22ae-d78f-c5963e7ef17d>
2014-01-24 23:42:36 +0000

    Dessin personnage

--- crushcolorsaga/character.cpp
+++ crushcolorsaga/character.cpp
@@ -1,5 +1,19 @@
 #include "character.h"
 
-Character::Character()
+#include <QPainter>
+
+
+Character::Character(int id) :
+    _id(id)
 {
+}
+
+QRectF Character::boundingRect() const
+{
+    return QRectF(0, 0, 32, 32);
+}
+
+void Character::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->drawPixmap(0, 0, QPixmap(QString(":/models/hero%1_standing").arg(_id)));
 }
