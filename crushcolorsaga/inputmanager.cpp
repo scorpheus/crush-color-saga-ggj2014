f107b6f0a35bc7d16efb3358d2f7d6ffecf067c8
wawanbreton@gmail.com <wawanbreton@gmail.com@ccb83103-6311-22ae-d78f-c5963e7ef17d>
2014-01-24 23:55:50 +0000

    codage

--- crushcolorsaga/inputmanager.cpp
+++ crushcolorsaga/inputmanager.cpp
@@ -1,6 +1,33 @@
 #include "inputmanager.h"
 
+#include <QCoreApplication>
+#include <QKeyEvent>
+
+
 InputManager::InputManager(QObject *parent) :
     QObject(parent)
 {
+    QCoreApplication::instance()->installEventFilter(this);
+}
+
+bool InputManager::eventFilter(QObject *, QEvent *event)
+{
+    if(event->type() == QEvent::KeyPress)
+    {
+        Qt::Key key = Qt::Key(((QKeyEvent *)event)->key());
+        if(key == Qt::Key_Up)
+        {
+
+        }
+        else if(key == Qt::Key_Left)
+        {
+
+        }
+        else if(key == Qt::Key_Right)
+        {
+
+        }
+    }
+
+    return false;
 }
--- crushcolorsaga/level.cpp
+++ crushcolorsaga/level.cpp
@@ -2,6 +2,8 @@
 
 #include "platform.h"
 #include "character.h"
+#include "inputmanager.h"
+
 
 Level::Level(QObject *parent) :
     QGraphicsScene(parent)
@@ -9,5 +11,12 @@ Level::Level(QObject *parent) :
     setSceneRect(0, 0, 427, 341);
 
     addItem(new Platform(QPoint(0, 341-16), 27));
-    addItem(new Character(1));
+
+    Character *character1 = new Character(1);
+    addItem(character1);
+
+    InputManager *inputManager = new InputManager(this);
+    connect(inputManager, SIGNAL(jump1()), character1, SLOT(jump()));
+    connect(inputManager, SIGNAL(moveLeft1()), character1, SLOT(moveLeft()));
+    connect(inputManager, SIGNAL(moveRight1()), character1, SLOT(moveRight()));
 }
