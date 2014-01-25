b430a093a395491997c93d94276b71624cb39aab
scorpheus.mr@gmail.com <scorpheus.mr@gmail.com@ccb83103-6311-22ae-d78f-c5963e7ef17d>
2014-01-25 01:58:31 +0000

    add game configurator dialog.

--- crushcolorsaga/gameconfigurationdialog.h
+++ crushcolorsaga/gameconfigurationdialog.h
@@ -2,6 +2,8 @@
 #define GAMECONFIGURATIONDIALOG_H
 
 #include <QDialog>
+#include <QTimer>
+#include <QGraphicsItem>
 
 namespace Ui { class GameConfigurationDialog; }
 
@@ -13,8 +15,26 @@ class GameConfigurationDialog : public QDialog
         explicit GameConfigurationDialog(QWidget *parent = 0);
         ~GameConfigurationDialog();
 
+    int _id_character1;
+    int _animationIndex1;
+    int _id_character2;
+    int _animationIndex2;
+
+    private slots:
+        void updateAnimation();
+
+        void on_pushButton_char1_prev_clicked();
+        void on_pushButton_char1_next_clicked();
+        void on_pushButton_char2_prev_clicked();
+        void on_pushButton_char2_next_clicked();
+
     private:
         Ui::GameConfigurationDialog *_ui;
+        QTimer *_timerAnimation;
+
+        static const int nb_character = 2;
+
+
 };
 
 #endif // GAMECONFIGURATIONDIALOG_H
