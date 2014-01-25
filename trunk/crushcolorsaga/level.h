28cc59674a75af1aac4838a26e8795c0c6f01dcf
guillaume.romand@gmail.com <guillaume.romand@gmail.com@ccb83103-6311-22ae-d78f-c5963e7ef17d>
2014-01-25 17:10:24 +0000

    Guillaume : intégration moteur physique. Non-fonctionnelle mais compile.

--- trunk/crushcolorsaga/level.h
+++ trunk/crushcolorsaga/level.h
@@ -2,6 +2,8 @@
 #define LEVEL_H
 
 #include <QGraphicsScene>
+#include <Box2d\Box2D.h>
+#include <Box2d\Collision\Shapes\b2PolygonShape.h>
 
 class Character;
 class EndLevel;
@@ -20,17 +22,24 @@ class Level : public QGraphicsScene
         virtual void CreateLevelPlatform()=0;
         virtual void FinishCreateLevel();
 
-    private:
+private:
         Character *character1;
         Character *character2;
 
         EndLevel* _end_level;
         Background *_background;
 
+        b2World*  world;
+
     public slots:
         void level_changed( const QList<QRectF> & region);
 
+protected:
+    void drawBackground ( QPainter * painter, const QRectF & rect );
+    virtual void timerEvent(QTimerEvent *event);
+
 private:
+
     QString _level_name;
 };
 
