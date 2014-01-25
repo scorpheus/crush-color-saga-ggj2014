c034b5ded55d21fa327e5ad841c4d6032cae3a58
scorpheus.mr@gmail.com <scorpheus.mr@gmail.com@ccb83103-6311-22ae-d78f-c5963e7ef17d>
2014-01-25 02:42:04 +0000

    add Level::GetBackgroundColor(QPoint pos)

--- crushcolorsaga/level.h
+++ crushcolorsaga/level.h
@@ -3,12 +3,32 @@
 
 #include <QGraphicsScene>
 
+enum ColorCharacter
+{
+    BLACK=0,
+    WHITE,
+    RED,
+    GREEN,
+    BLUE,
+    PURPLE,
+    CYAN,
+    YELLOW
+};
+
 class Level : public QGraphicsScene
 {
     Q_OBJECT
 
     public:
-        explicit Level(QObject *parent = 0);
+        explicit Level(QString level_name, QObject *parent = 0);
+
+        ColorCharacter GetBackgroundColor(QPoint pos);
+
+protected:
+    void drawBackground ( QPainter * painter, const QRectF & rect );
+
+private:
+    QString _level_name;
 };
 
 #endif // LEVEL_H
